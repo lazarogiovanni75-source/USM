86dfb316fd1a52b31c8e3147dc0fd0d4a8364c88
Clacky <develop@clacky.ai>
2026-03-29 23:57:33 -0400

    Fix manifest - only link specific CSS files, vendor CSS now bundled into application

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,5 @@
 //= link_tree ../builds
-//= link_directory ../stylesheets .css
+//= link application.css
+//= link admin.css
 //= link_directory ../javascripts .js
 //= link_tree ../images
-
-//= link node_modules/tom-select/dist/css/tom-select.min.css
-//= link node_modules/flatpickr/dist/flatpickr.min.css
-//= link node_modules/tom-select/dist/js/tom-select.complete.min.js
-//= link node_modules/flatpickr/dist/flatpickr.min.js
