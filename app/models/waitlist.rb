45244a60d855c59a7194f56d991f8a1180799255
lazarogiovanni75-source <lazarogiovanni75@gmail.com>
2026-03-31 21:35:06 -0700

    Delete app/models/waitlist.rb

--- app/models/waitlist.rb
+++
@@ -1,6 +0,0 @@
-class WaitlistEmail < ApplicationRecord
-  self.table_name = 'waitlists'
-
-  validates :email, presence: true, uniqueness: { case_sensitive: false },
-                    format: { with: URI::MailTo::EMAIL_REGEXP }
-end
