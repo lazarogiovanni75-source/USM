304b6d266335c230d3ad0b1f61bd41a796f94a4a
lazarogiovanni75-source <lazarogiovanni75@gmail.com>
2026-03-31 22:08:28 -0700

    Set custom table name for WaitlistEmail model

--- app/models/waitlist_email.rb
+++ app/models/waitlist_email.rb
@@ -1,4 +1,6 @@
 class WaitlistEmail < ApplicationRecord
+  self.table_name = 'waitlist_emails'
+
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     format: { with: URI::MailTo::EMAIL_REGEXP }
 end
