e243be72f7915f7978af476bd53de475ce02f204
lazarogiovanni75-source <lazarogiovanni75@gmail.com>
2026-03-31 21:19:33 -0700

    Rename Waitlist class to WaitlistEmail

--- app/models/waitlist.rb
+++ app/models/waitlist.rb
@@ -1,6 +1,6 @@
-class Waitlist < ApplicationRecord
+class WaitlistEmail < ApplicationRecord
   self.table_name = 'waitlists'
-  
+
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     format: { with: URI::MailTo::EMAIL_REGEXP }
 end
