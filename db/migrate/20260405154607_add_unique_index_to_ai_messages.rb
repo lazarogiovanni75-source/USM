10cdf7a0b89325945ead9ccc646602dec560a0b2
lazarogiovanni75-source <lazarogiovanni75@gmail.com>
2026-04-05 21:24:03 -0700

    Delete db/migrate/20260405154607_add_unique_index_to_ai_messages.rb

--- db/migrate/20260405154607_add_unique_index_to_ai_messages.rb
+++
@@ -1,5 +0,0 @@
-class AddUniqueIndexToAiMessages < ActiveRecord::Migration[7.2]
-  def change
-    add_index :ai_messages, :id, unique: true unless index_exists?(:ai_messages, :id, unique: true)
-  end
-end
