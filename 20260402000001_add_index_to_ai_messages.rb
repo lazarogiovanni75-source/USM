402e9b35344509b3ca58d8631aa2551ce2482e4e
lazarogiovanni75-source <lazarogiovanni75@gmail.com>
2026-04-05 21:24:57 -0700

    Delete 20260402000001_add_index_to_ai_messages.rb

--- 20260402000001_add_index_to_ai_messages.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexToAiMessages < ActiveRecord::Migration[7.2]
-  def change
-    unless index_exists?(:ai_messages, :id, unique: true)
-      add_index :ai_messages, :id, unique: true
-    end
-  end
-end
