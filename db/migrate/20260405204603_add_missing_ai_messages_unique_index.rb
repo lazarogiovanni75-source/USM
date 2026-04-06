31b6657d5e4c57d38acc58680e8ec7d66cdccf19
lazarogiovanni75-source <lazarogiovanni75@gmail.com>
2026-04-05 21:26:45 -0700

    Delete db/migrate/20260405204603_add_missing_ai_messages_unique_index.rb

--- db/migrate/20260405204603_add_missing_ai_messages_unique_index.rb
+++
@@ -1,8 +0,0 @@
-class AddMissingAiMessagesUniqueIndex < ActiveRecord::Migration[7.2]
-  def change
-    # Use raw SQL to ensure the unique index exists - bypasses Rails migration issues
-    unless index_exists?(:ai_messages, :id, unique: true)
-      add_index :ai_messages, :id, unique: true
-    end
-  end
-end
