8058f8b49141cf65e185d785bbd9424698a9bb47
Clacky <develop@clacky.ai>
2026-04-08 00:41:36 -0400

    Fix migration timestamp - use valid current timestamp instead of future date

--- db/migrate/20260408004130_add_unique_index_to_ai_messages_id.rb
+++ db/migrate/20260408004130_add_unique_index_to_ai_messages_id.rb
@@ -1,6 +1,6 @@
 class AddUniqueIndexToAiMessagesId < ActiveRecord::Migration[7.2]
   def change
-    unless index_exists?(:ai_messages, :id)
+    unless index_exists?(:ai_messages, :id, unique: true)
       add_index :ai_messages, :id, unique: true, name: :index_ai_messages_on_id
     end
   end
