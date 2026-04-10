1b05fe562df0d198465139a582a041bbb55406cd
Clacky <develop@clacky.ai>
2026-04-10 15:43:15 -0400

    Fix missing set_default_headers method causing 500 errors in API controller

--- app/controllers/api/v1/workflows_controller.rb
+++ app/controllers/api/v1/workflows_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::WorkflowsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :set_default_headers
   before_action :check_workflow_access, only: [:create, :index]
 
   def check_workflow_access
