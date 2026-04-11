542094ec0c84003c6fc5cab79ef54641e1af7dab
Clacky <develop@clacky.ai>
2026-04-11 01:43:52 -0400

    Remove redundant migration (columns already in create_workflows)

--- db/migrate/20260411041200_add_content_to_workflows.rb
+++
@@ -1,8 +0,0 @@
-class AddContentToWorkflows < ActiveRecord::Migration[7.2]
-  def change
-    add_column :workflows, :content, :text
-    add_column :workflows, :title, :string
-    add_column :workflows, :error_message, :text
-
-  end
-end
