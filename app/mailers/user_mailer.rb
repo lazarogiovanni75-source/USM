85ab285686cf28fe69b2e891cf0645065a524675
Clacky <develop@clacky.ai>
2026-04-28 22:18:18 -0400

    Add charset UTF-8 and content_type to mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,12 @@
 class UserMailer < ApplicationMailer
+  default content_type: 'text/html', charset: 'UTF-8'
+
   def password_reset
     @user = params[:user]
     @signed_id = @user.generate_token_for(:password_reset)
 
     app_name = Rails.application.config.x.appname.encode('UTF-8', invalid: :replace, undef: :replace, replace: '')
-    mail to: @user.email, subject: "[#{app_name}] Reset your password"
+    mail to: @user.email, subject: "[#{app_name}] Reset your password", content_type: 'text/html; charset=UTF-8'
   end
 
   def email_verification
@@ -12,7 +14,7 @@ class UserMailer < ApplicationMailer
     @signed_id = @user.generate_token_for(:email_verification)
 
     app_name = Rails.application.config.x.appname.encode('UTF-8', invalid: :replace, undef: :replace, replace: '')
-    mail to: @user.email, subject: "[#{app_name}] Verify your email"
+    mail to: @user.email, subject: "[#{app_name}] Verify your email", content_type: 'text/html; charset=UTF-8'
   end
 
   def invitation_instructions
@@ -20,6 +22,6 @@ class UserMailer < ApplicationMailer
     @signed_id = @user.generate_token_for(:password_reset)
 
     app_name = Rails.application.config.x.appname.encode('UTF-8', invalid: :replace, undef: :replace, replace: '')
-    mail to: @user.email, subject: "[#{app_name}] Invitation instructions"
+    mail to: @user.email, subject: "[#{app_name}] Invitation instructions", content_type: 'text/html; charset=UTF-8'
   end
 end
